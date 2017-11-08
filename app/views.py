850626e9b681c5f896c045915bd5b97ca04a95a4
LukaGabriel <30847748+LukaGabriel@users.noreply.github.com>
2017-11-08 21:39:37 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -83,5 +83,5 @@ def lista_candidatos(request):
         context_instance = RequestContext(request,
         {
             'title':'Cadastro de candidatos',
-            'localProva': Candidato.objects.all(),
+            'candidatos': Candidato.objects.all(),
             'year':datetime.now().year,
